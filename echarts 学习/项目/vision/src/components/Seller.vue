8270368d98eaf2b9b790015937168dbba79ef8d3
liujiaqi222 <z473487465@qq.com>
2021-10-11 08:06:49 +0800

    update

--- echarts 学习/项目/vision/src/components/Seller.vue
+++ echarts 学习/项目/vision/src/components/Seller.vue
@@ -1,43 +1,71 @@
 <!-- 商家销量统计的横向统计图 -->
 <template>
   <div class="com-container">
-      <div class="com-chart" ref="seller_ref">
-12222222222222
-      </div>
+    <div class="com-chart" ref="seller_ref">12222222222222</div>
   </div>
 </template>
 
 <script>
 export default {
-    data(){
-        return {
-            chartInstance:null,
-        }
+  data() {
+    return {
+      chartInstance: null,
+      allData: null, //服务器返回的数据
+      currentPage: 1, //当前显示的页数
+      totalPage: 0, //总页数
+    };
+  },
+  methods: {
+    // 初始化echartsInstance 对象
+    initChart() {
+      this.chartInstance = this.$echarts.init(this.$refs.seller_ref);
     },
-    methods:{
-        // 初始化echartsInstance 对象
-        initChart(){
-            console.log(this.$echarts);
-},
-        // 获取数据
-        async getData(){
-           const ret = await this.$axios.get('/seller');
-           console.log(ret);
+    // 获取数据
+    async getData() {
+      const { data } = await this.$axios.get("/seller");
+      this.allData = data;
+      // 对数组进行排序
+      this.allData.sort((a, b) => {
+        return a.value - b.value;
+      });
+      // 每5个元素显示一页，判断总页数
+      this.totalPage = Math.cell(this.allData.length / 5);
+      this.updateChart();
+    },
+    // 更新图表
+    updateChart() {
+      const showData = this.allData;
+      const sellerNames = showData.map((item) => item.name);
+      const sellerValues = showData.map((item) => item.value);
+      const option = {
+        xAxis: {
+          type: "value",
         },
-        // 更新图表
-        updateChart() {
-            
+        yAxis: {
+          type: "category",
+          data: sellerNames,
         },
+        series: [
+          {
+            type: "bar",
+            data: sellerValues,
+          },
+        ],
+      };
+      this.chartInstance.setOption(option);
     },
-    mounted(){
-        this.initChart();
-    },
-    created(){
-        this.getData();
-    }
-}
+  },
+  mounted() {
+    this.initChart();
+  },
+  created() {
+    this.getData();
+  },
+};
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+.com-chart {
+  height: 600px;
+}
 </style>
--- echarts 学习/项目/vision/src/main.js
+++ echarts 学习/项目/vision/src/main.js
@@ -1,10 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import echarts from 'echarts'
 import axios from 'axios'
 import './assets/css/global.css'
-
+// 5.0需要这样引入echarts
+import * as echarts from 'echarts'
 Vue.config.productionTip = false
 
 // 将全局的echarts对象挂载到vue原型实例上
