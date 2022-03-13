680f7db4fb2a82aa19c72bf6f36bf46efd03dca5
liujiaqi222 <2473487465@qq.com>
2022-03-13 23:59:15 +0800

    update

--- React/review/src/index.js
+++ React/review/src/index.js
@@ -1,19 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import PropTypes from 'prop-types';
 
-class Hello extends React.Component{
-  constructor(props){
-    super(props);
-    console.log(props)
-  }
-  render(){
-    return (
-      <div>
-        接收到的数据：{this.props.age}
-      </div>
-    )
-  }
+
+function App(props){
+  const lis = props.colors.map(item => <li key={item}>{item}</li>)
+  return (
+    <div>
+      HI {props.colors}
+      {props.pageSize}
+      <ul>{lis}</ul>
+    </div>
+  );
 }
 
-ReactDOM.render(<Parent/>, document.getElementById("root"));
+App.propTypes = {
+  colors:PropTypes.array
+}
+App.defaultProps = {
+  pageSize:10
+}
+
+
+ReactDOM.render(<App colors={['red','blue']}/>, document.getElementById("root"));
