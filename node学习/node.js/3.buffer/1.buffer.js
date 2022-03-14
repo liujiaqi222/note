4fc22cd8837f713bfd4593cb42ad79c879211d40
liujiaqi222 <2473487465@qq.com>
2022-03-14 23:44:06 +0800

    update

---
+++ node学习/node.js/3.buffer/1.buffer.js
@@ -0,0 +1,27 @@
+// buffer是Nodejs全局对象上的一个类，是一个专门用于存储字节数据的类
+// Nodejs提供了操作计算机底层的api，而计算机底层只能识别0和1
+// 所以就提供了一个专门用于存储字节数据的类
+
+
+
+// 1. 创建一个buffer对象
+//Buffer.alloc(size[, fill[, encoding]])
+let buf = Buffer.alloc(5,12)
+
+// Note: 通过console.log()输出buffer，会自动将存储的内容转换为16进制再输出
+console.log(buf) //<Buffer 0c 0c 0c 0c 0c>
+
+// 2. 也可以根据数组或者字符串创建一个buffer对象
+console.log(Buffer.from('abc')) //<Buffer 61 62 63> 转换成十进制就是97 98 99
+console.log(Buffer.from([1,2,3])) //<Buffer 01 02 03>
+
+// 3. buffer的本质是数组
+console.dir(buf) //Buffer(5) [Uint8Array] [ 12, 12, 12, 12, 12 ]
+
+// 因此可以像数组一样来修改buffer
+buf[0] =1
+console.log(buf) //<Buffer 01 0c 0c 0c 0c>
+
+
+
+
---
+++ node学习/node.js/3.buffer/2.buffer常用的实例方法.js
@@ -0,0 +1,16 @@
+// 1. 将二进制数据转换成字符串
+let buf = Buffer.from([97,98,99]);
+console.log(buf.toString()); //abc
+
+// 2. 往buffer中写入数据
+// buf.write(string[, offset[,length[,encoding]])
+buf = Buffer.alloc(5);
+buf.write('abcdefg');
+console.log(buf,buf.toString()); //<Buffer 61 62 63 64 65>  abcde
+//只会保留前5位，因为只分配了5位的空间
+
+
+// 3.从指定位置截取新的buffer
+// buf.slice([start[, end]])
+console.log(buf.slice(2,4)); //<Buffer 61 62 63 64 65> 
+
