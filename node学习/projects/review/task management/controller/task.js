a974694f9fc33d3ae4af6a401d32fc4dd57373b7
liujiaqi222 <2473487465@qq.com>
2022-04-22 07:46:04 +0800

    111

--- node学习/projects/review/task management/controller/task.js
+++ node学习/projects/review/task management/controller/task.js
@@ -1,11 +1,17 @@
+const Task = require("../models/task");
+
 const getAllTasks = (req, res) => {
   res.send("all items");
 };
 
-
 // post: create new task
-const createTask = (req, res) => {
-  res.send("create a new task");
+const createTask = async (req, res) => {
+  try {
+    const task = await Task.create(req.body);
+    res.status(201).json(task);
+  } catch (err) {
+    res.status(500).json({ message: err });
+  }
 };
 // get a task
 const getTask = (req, res) => {
--- node学习/projects/review/task management/models/task.js
+++ node学习/projects/review/task management/models/task.js
@@ -2,8 +2,19 @@ const mongoose = require("mongoose");
 
 // 设置文档的数据结构
 const TaskSchema = new mongoose.Schema({
-  name: String,
-  completed: Boolean,
+  name: {
+    type: String,
+    requierd: [true, "必须提供任务的名字"],
+    trim: true,
+    maxlength: [20, "名字的长度不能超过20"],
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
 });
 
 //只有符合schema的数据才会被添加到数据库，其他的都会被忽略
+const Task = mongoose.model("Task", TaskSchema);
+
+module.exports = Task;
